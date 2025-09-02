608cabcd8488185d68a43f1fd8b99af867773a07
AliHussainKhamis <alihussainkh3@gmail.com>
2025-09-02 14:30:29 +0300

    horse details

--- controllers/horse.js
+++ controllers/horse.js
@@ -18,7 +18,21 @@ async function getHorse(req,res){
 }
 
 // Details
-
+async function showHorse(req,res){
+    try{
+        const horse = await Horse.findById()
+        if(horse){
+            res.status(200).json(horse)
+        }
+        else{
+            res.sendStatus(204)
+        }
+    }
+    catch(error){
+        console.log("Error in finding Horses:",error)
+        res.status(500).json({error: error.message})
+    }
+}
 // Create
 
 // Update
