85360905189010729634d76f515d86d80ede3e7b
AliHussainKhamis <alihussainkh3@gmail.com>
2025-09-03 13:00:19 +0300

    User instead of Owner

--- models/Owner.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const ownerSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
-  email: String,
-})
-
-module.exports = mongoose.model("Owner", ownerSchema)
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+
+  role: { type: String, enum: ['admin', 'owner'], default: 'owner' },
+
+  phone: String,
+  address: String,
+}, { timestamps: true })
+
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.passwordHash)
+}
+
+module.exports = mongoose.model('User', userSchema)
